f787892436cf8ce8b0ca05ebce4458452211510c
melheno271 <70054092+melheno271@users.noreply.github.com>
2024-08-08 17:18:07 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,3 @@
-module "sandbox_02" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "melkonyanhenrik+sandbox02@gmail.com"
-    AccountName               = "sandbox-aft-02"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "melkonyanhenrik+sandbox02@gmail.com"
-    SSOUserFirstName          = "Sandbox02"
-    SSOUserLastName           = "AFT02"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
 module "sandbox" {
   source = "./modules/aft-account-request"
 
@@ -45,6 +17,34 @@ module "sandbox" {
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "development" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "melkonyanhenrik+development@gmail.com"
+    AccountName               = "development"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "melkonyanhenrik+development@gmail.com"
+    SSOUserFirstName          = "Development"
+    SSOUserLastName           = "AFTDEV"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "dev-AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Vending Development Account 02"
   }
 
   custom_fields = {
